36591546d50da9ed756a9de573f461495275330f
kumavale <kumavale.hyperion@gmail.com>
2023-09-01 22:24:07 +0900

    feat: Add echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:12345").await?;
+
+    let (mut socket, _) = listener.accept().await?;
+    loop {
+        let mut buf = vec![];
+        socket.read_buf(&mut buf).await?;
+        socket.write_all(&buf).await?;
+    }
 }
