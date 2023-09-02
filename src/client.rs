d0700c68978a0bf6b3e763ee0d0f3d4978ec5b2a
kumavale <kumavale.hyperion@gmail.com>
2023-09-02 21:54:56 +0900

    feat: When the server side ends, the client side also ends.

--- src/client.rs
+++ src/client.rs
@@ -1,30 +1,38 @@
-use anyhow::Context;
+use anyhow::{anyhow, Context};
 use std::io::{stdout, Write};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::net::TcpStream;
-use tokio::sync::oneshot;
+use tokio::sync::broadcast;
 
 pub async fn run(destination: String, port: Option<u16>) -> anyhow::Result<()> {
     let port = port.context("missing port number")?;
     let stream = TcpStream::connect(format!("{destination}:{port}")).await?;
     let (stream, sink) = stream.into_split();
+
+    let (stdin, proxy) = broadcast::channel(1);
+    let input_handle = tokio::task::spawn_blocking(move || -> anyhow::Result<()> {
+        loop {
+            let mut buf = String::new();
+            std::io::stdin().read_line(&mut buf)?;
+            stdin.send(buf).map_err(|e| anyhow!(e))?;
+        }
+    });
+
     tokio::select! {
-        _ = tx(sink) => (),
-        _ = rx(stream) => (),
+        _ = input_handle => tracing::debug!("input end"),
+        _ = tx(sink, proxy) => tracing::debug!("tx end"),
+        _ = rx(stream) => tracing::debug!("rx end"),
     }
     Ok(())
 }
 
-async fn tx(mut sink: OwnedWriteHalf) -> anyhow::Result<()> {
+async fn tx(
+    mut sink: OwnedWriteHalf,
+    mut proxy: broadcast::Receiver<String>,
+) -> anyhow::Result<()> {
     loop {
-        let (stdin, proxy) = oneshot::channel();
-        tokio::task::spawn_blocking(move || {
-            let mut buf = String::new();
-            std::io::stdin().read_line(&mut buf).unwrap();
-            stdin.send(buf).unwrap();
-        });
-        let buf = proxy.await?;
+        let buf = proxy.recv().await?;
         sink.write_all(buf.as_bytes()).await?;
     }
 }
@@ -32,8 +40,13 @@ async fn tx(mut sink: OwnedWriteHalf) -> anyhow::Result<()> {
 async fn rx(mut stream: OwnedReadHalf) -> anyhow::Result<()> {
     loop {
         let mut buf = vec![];
-        stream.read_buf(&mut buf).await?;
-        print!("{}", String::from_utf8_lossy(&buf));
-        stdout().flush()?;
+        match stream.read_buf(&mut buf).await {
+            Ok(0) => return Ok(()),
+            Ok(_) => {
+                print!("{}", String::from_utf8_lossy(&buf));
+                stdout().flush()?;
+            }
+            Err(e) => anyhow::bail!(e),
+        }
     }
 }
